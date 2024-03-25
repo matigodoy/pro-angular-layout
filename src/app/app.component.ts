7214291b2c713c24e38f92dcbc03e8d47c75cad4
Matias Godoy <mati_godoy@outlook.com>
2024-03-25 00:53:11 -0300

    Add new files for login, signup, and recover password components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,34 @@
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
-import { Component } from '@angular/core';
+import { TuiThemeNightModule, TuiModeModule } from '@taiga-ui/core';
+import { Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TuiBrightness } from '@taiga-ui/core';
+import { TuiThemeNightService } from '@taiga-ui/addon-doc/services';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiThemeNightModule,
+    TuiModeModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }]
 })
 export class AppComponent {
-  title = 'pro-angular-layout';
+  constructor(@Inject(TuiThemeNightService) readonly night: TuiThemeNightService) {
+    // night.toggle(); con esto se pone modo claro
+  }
+
+  get mode(): TuiBrightness | null {
+    return this.night.value ? 'onDark' : null;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/auth/login',
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+    },
+];
